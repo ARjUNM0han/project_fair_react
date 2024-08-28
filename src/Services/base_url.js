5dddfa5490837fbb3c63dfe39859aa0aa5b3a419
ArjunMohan <arjunmohan054@gmail.com>
2024-08-28 14:47:25 +0530

    base url updation

--- src/Services/base_url.js
+++ src/Services/base_url.js
@@ -1,4 +1,4 @@
 // const base_url = 'http://localhost:3000'
-const base_url = 'https://project-fair-server-d4s4.onrender.com/'
+const base_url = 'https://project-fair-server-d4s4.onrender.com'
 
 export default base_url
